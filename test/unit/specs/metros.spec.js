2109b5057ed44fe952e1ec0fa53ad38bca50e00c
sfmiller940 <sfmiller940@gmail.com>
2019-08-20 22:24:05 -0700

    Cleanup

--- test/unit/specs/metros.spec.js
+++ test/unit/specs/metros.spec.js
@@ -4,18 +4,18 @@ import metros from '../../../src/metros.js'
 describe('metros Object', () => {
   test('has keys of type string', () => {
     for( let id in metros ){
-      expect(typeof id).toEqual('string')
+      expect(typeof id).toBe('string')
     }
   })
   test('has station ids as keys',()=>{
     for( let id in metros ){
-      expect(id).toEqual(metros[id].id)
+      expect(id).toBe(metros[id].id)
     }    
   })
   test('contains station properties',()=>{
     for( let id in metros ){
-      expect(typeof metros[id].name).toEqual('string')
-      expect(typeof metros[id].city).toEqual('string')
+      expect(typeof metros[id].name).toBe('string')
+      expect(typeof metros[id].city).toBe('string')
       expect(typeof metros[id].coords.lat).toBe('number')
       expect(typeof metros[id].coords.lng).toBe('number')
       expect(typeof metros[id].getStations).toBe('function')
