a3a8b2709d0befaf0651c01b18556013ce4b00be
sfmiller940 <sfmiller940@gmail.com>
2019-08-30 17:48:55 -0700

    Move gmap apiKey logic out of private config.js to public index.js

--- src/config/index.js
+++ src/config/index.js
@@ -8,6 +8,13 @@ else{
   apiKeys = require('./apiKeys').default
 }
 
+apiKeys.gmap = ( 
+  process.env.NODE_ENV == 'development' ? apiKeys.gmap.dev :
+    ( process.env.NODE_ENV == 'production' ? apiKeys.gmap.prod :
+      apiKeys.gmap.test
+    )
+)
+
 const config = {
   apiKeys: apiKeys,
   testURL: 'http://www.poibella.org'
