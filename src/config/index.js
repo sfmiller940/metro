e57b516d9537495fe3ac25990e6b6c01026d7cac
sfmiller940 <sfmiller940@gmail.com>
2019-08-30 17:26:48 -0700

    Add check for env config

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,12 @@
-import apiKeys from './apiKeys'
+//import apiKeys from './apiKeys'
+
+let apiKeys = {}
+if(process.env.apiKeys){
+  apiKeys = JSON.parse(process.env.apiKeys)
+}
+else{
+  apiKeys = require('./apiKeys').default
+}
 
 const config = {
   apiKeys: apiKeys,
